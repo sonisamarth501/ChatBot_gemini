4a8b8509f9c88571a4d8edf21e6a08e3be944b6e
samarth soni <sonisamarth501@gmail.com>
2025-06-27 23:58:38 +0530

    initial commit

--- lib/core/constants/api_const.dart
+++
@@ -1,4 +0,0 @@
-class StringConst {
-  static const apiUrl =
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCPUM8fYRqaUzJSLgOmhv-5t6S5pgnbMdw";
-}
